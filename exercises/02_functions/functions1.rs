eb9bc8ca252a5a9eef59942c0b7c0a18064e63b8
Sofia Vazquez <sofia.vazquez.duarte@tecnico.ulisboa.pt>
2026-02-09 21:27:13 +0000

    rustlings advancements

---
+++ exercises/02_functions/functions1.rs
@@ -0,0 +1,11 @@
+// TODO: Add some function with the name `call_me` without arguments or a return value.
+
+
+fn call_me(){
+    let mut x = 1;
+    x += 2;
+    println!("The value of x is {x}");
+}
+fn main() {
+    call_me(); // Don't change this line
+}
